1ac88e571619d7562c601baad261e1e5d990e959
BuildTools <unconfigured@null.spigotmc.org>
2020-06-02 21:45:02 +0200

    made a search system

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
-import HeroCard from './components/HeroCard'
-import logo from './logo.svg';
-import './App.css';
-import { HeroWrapper } from './types';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import HeroCard from "./components/HeroCard";
+import "./App.css";
+import { HeroWrapper } from "./types";
 import styled, { ThemeProvider } from "styled-components";
 import { theme } from "./styles/default-theme";
 function App() {
-  const [champions, setChampions] = useState({} as HeroWrapper)
+  const [champions, setChampions] = useState({} as HeroWrapper);
+  const [filter, setFilter] = useState("");
+
   useEffect(() => {
     async function fetchHeroes() {
-      const res = await axios.get("https://ddragon.leagueoflegends.com/cdn/10.5.1/data/en_US/champion.json")
-      const data = res.data
-      setChampions(data.data)
+      const res = await axios.get(
+        "https://ddragon.leagueoflegends.com/cdn/10.5.1/data/en_US/champion.json"
+      );
+      const data = res.data;
+      setChampions(data.data);
     }
     fetchHeroes();
-  }, [])
+  }, []);
+
   return (
-    <ThemeProvider theme={theme}><HeroCardCollection>
-      {Object.keys(champions).map(champ => <HeroCard key={champions[champ].key} name={champions[champ].id} blurb={champions[champ].blurb} image={champions[champ].image} />)}
-    </HeroCardCollection>
+    <ThemeProvider theme={theme}>
+      <div className="searchbar">
+        <SearchLabel htmlFor="search">Search for a champion</SearchLabel>
+        <SearchInput
+          type="text"
+          onChange={(e) => {
+            setFilter(e.target.value.toLowerCase());
+          }}
+        />
+      </div>
+      <HeroCardCollection>
+        {Object.keys(champions)
+          .filter((c) => c.toLowerCase().includes(filter))
+          .map((champ) => (
+            <HeroCard
+              key={champions[champ].key}
+              name={champions[champ].id}
+              blurb={champions[champ].blurb}
+              image={champions[champ].image}
+            />
+          ))}
+      </HeroCardCollection>
     </ThemeProvider>
   );
 }
@@ -27,7 +50,17 @@ function App() {
 const HeroCardCollection = styled.section`
   display: flex;
   flex-wrap: wrap;
-  background-color: black;
-`
+`;
+
+const SearchLabel = styled.label`
+  font-size: 32px;
+  padding: 20px;
+  color: white;
+`;
+const SearchInput = styled.input`
+  color: purple;
+  background-color: green;
+  font-size: 32px;
+`;
 
 export default App;
--- src/components/HeroCard.tsx
+++ src/components/HeroCard.tsx
@@ -1,40 +1,40 @@
-import React from 'react'
-import { Hero } from '../types'
-import styled from 'styled-components'
+import React from "react";
+import { Hero } from "../types";
+import styled from "styled-components";
 
 //version, id, key, name, title, blurb, info, Image, tags, partype, stats,
 //<Paragraph>{props.blurb}</Paragraph>
 
 const HeroCard: React.FC<Hero> = (props) => {
-    return (
-        <Card>
-            <Title>{props.name}</Title>
-            <a href={`https://na.leagueoflegends.com/en-us/champions/${props.name?.toLocaleLowerCase()}`}><Image src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${props.name}_0.jpg`}></Image></a>
-            {console.log(props)}
-        </Card>
-    )
-}
+  return (
+    <Card>
+      <Title>{props.name}</Title>
+      <a
+        href={`https://na.leagueoflegends.com/en-us/champions/${props.name?.toLocaleLowerCase()}`}
+      >
+        <Image
+          src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${props.name}_0.jpg`}
+        ></Image>
+      </a>
+    </Card>
+  );
+};
 
 const Title = styled.h1`
-    color: white;
-`
-
-const Paragraph = styled.p`
-    width: 12rem;
-    margin: 2rem;
-    color: white;
-`
-
-const Card = styled.div`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    margin: 3rem;
-    width: 5rem;
-`
-
-const Image = styled.img`
-    width: 10rem;
+  color: white;
+  font-family: Georgia, "Times New Roman", Times, serif;
 `;
 
-export default HeroCard
+const Card = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 3rem;
+  width: 5rem;
+`;
+
+const Image = styled.img`
+  width: 10rem;
+`;
+
+export default HeroCard;
