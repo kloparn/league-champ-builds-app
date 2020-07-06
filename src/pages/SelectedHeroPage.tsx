ab642c37c29c60d930a91c5ee24ddb88b26a2c4c
kloparn <adam97.live.com@live.com>
2020-07-06 11:18:46 +0200

    updated so it now displays the current champs picture

--- src/pages/SelectedHeroPage.tsx
+++ src/pages/SelectedHeroPage.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Hero } from "../types";
+import { HeroWrapper } from "../types";
 import axios from "axios";
 
-const SelectedHeroPage: React.FC<Hero> = () => {
-  const [hero, setHero] = useState({} as Hero);
+const SelectedHeroPage: React.FC = () => {
+  const [hero, setHero] = useState({} as HeroWrapper);
 
   useEffect(() => {
     const getCurrentHero = async () => {
@@ -19,13 +19,76 @@ const SelectedHeroPage: React.FC<Hero> = () => {
 
     getCurrentHero();
   }, []);
-  console.log(hero.name);
 
-  return <Wrapper id={hero.id}>Currently selected {hero.name}</Wrapper>;
+  return (
+    <Wrapper>
+      {Object.keys(hero).map((name) => (
+        <HeroShowcase key={hero[name].key}>
+          <img
+            src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${
+              hero[name].image?.full.split(".")[0]
+            }_0.jpg`}
+            alt={hero[name].blurb}
+          />
+          {hero[name].name}
+        </HeroShowcase>
+      ))}
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
   color: white;
+`;
+const HeroShowcase = styled.section`
+  animation: fadeIn ease 5s;
+  -webkit-animation: fadeIn ease 5s;
+  -moz-animation: fadeIn ease 5s;
+  -o-animation: fadeIn ease 5s;
+  -ms-animation: fadeIn ease 5s;
+  @keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+@-moz-keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+@-webkit-keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+@-o-keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+@-ms-keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+}
 `;
 
 export default SelectedHeroPage;
